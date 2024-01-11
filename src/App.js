229e7444ed418168b3f3574ca15b6d992a3cbddf
sowmiyalakshmi44 <sowmiyalakshmi44@gmail.com>
2024-01-11 18:43:42 +0530

    userprofile

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Route,Routes} from "react-router-dom"
 
 const App = () => {
   return (
-   <AnimatePresence>
+   <AnimatePresence  mode="wait">
     <div className="w-screen h-auto flex flex-col bg-primary">
         <Header />
         <main className="mt-24 p-8 w-full">
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+// import React from "react";
 import Logo from "../img/logo.png";
-import ShoppingBasketSharpIcon from "@mui/icons-material/ShoppingBasketSharp";
+import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase.config";
 
@@ -8,6 +8,8 @@ import Avatar from "../img/avatar.png";
 import { motion } from "framer-motion";
 // import { Scale } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
+
 import { useStateValue } from "../Context/StateProvider";
 import { actionType } from "../Context/reducer";
 
@@ -33,6 +35,9 @@ const Header = () => {
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
+  const [isMenu, setIsMenu] = useState(false);
+
+
   const [{ user }, dispatch] = useStateValue();
   const login = async () => {
     // let response = await signInWithPopup(firebaseAuth, provider);
@@ -48,7 +53,19 @@ const Header = () => {
       localStorage.setItem("user", JSON.stringify(providerData[0]));
 
     }
+    else {
+      setIsMenu(!isMenu);
+    }
     
+  };
+  const logout = () => {
+    setIsMenu(false);
+    localStorage.clear();
+
+    dispatch({
+      type: actionType.SET_USER,
+      user: null,
+    });
   };
 
   return (
@@ -59,7 +76,10 @@ const Header = () => {
           <p className="text-headingColor text-xl font-bold">city</p>
         </Link>
         <div className="flex items-center gap-8">
-          <ul className="flex items-center gap-8 ml-auto">
+          <motion.ul initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 200 }}
+             className="flex items-center gap-8 ml-auto">
             {[...HEADER_MENU].map((item) => {
               const { name = "", id = "" } = item || {};
               return (
@@ -71,9 +91,9 @@ const Header = () => {
                 </li>
               );
             })}
-          </ul>
+          </motion.ul>
           <div className="relative flex items-center justify-center">
-            <ShoppingBasketSharpIcon className="text-textColor text-2xl cursor-pointer" />
+            <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
             <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
               <p className="text-sm text-white font-semibold">2</p>
             </div>
@@ -86,6 +106,32 @@ const Header = () => {
               alt="userprofile"
               onClick={login}
             />
+              {isMenu && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.6 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.6 }}
+                className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
+              >
+                {user && user.email === "sowmiya.2005139@srec.ac.in" && (
+                  <Link to={"/createItem"}>
+                    <p
+                      className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                      onClick={() => setIsMenu(false)}
+                    >
+                      New Item <MdAdd />
+                    </p>
+                  </Link>
+                )}
+
+                <p
+                  className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                  onClick={logout}
+                >
+                  Logout <MdLogout />
+                </p>
+              </motion.div>
+            )}
           </div>
         </div>
       </div>
