c9d6bd860fb3cdea25def107f51d294a372fa052
sowmiyalakshmi44 <sowmiyalakshmi44@gmail.com>
2024-02-08 21:02:32 +0530

    carshow

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -67,7 +67,12 @@ const Header = () => {
       user: null,
     });
   };
-
+  const showCartIcon = () => {
+    dispatch({
+      type: actionType.SET_CART_SHOW,
+      cartShow: !cartShow,
+    });
+  };
   return (
     <header className="fixed z-50 w-screen  p-3 px-4 md:p-6 md:px-16 bg-primary">
       <div className="hidden md:flex w-full h-full items-center justify-between">
@@ -94,7 +99,7 @@ const Header = () => {
               );
             })}
           </motion.ul>
-          <div className="relative flex items-center justify-center">
+          <div className="relative flex items-center justify-center " onClick={showCartIcon}>
             <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
             <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
               <p className="text-sm text-white font-semibold">2</p>
@@ -139,7 +144,7 @@ const Header = () => {
       </div>
 
       {/* mobile */}
-      <div className="flex md:hidden w-full h-full">
+      <div className="flex md:hidden w-full h-full" onClick={showCart}>
         <div
           className="relative flex items-center justify-center"
           onClick={showCart}
@@ -174,7 +179,7 @@ const Header = () => {
               exit={{ opacity: 0, scale: 0.6 }}
               className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
             >
-              {user && user.email === "vetrivel.galaxy@gmail.com" && (
+              {user && user.email === "sowmiya.2005139@srec.ac.in" && (
                 <Link to={"/createItem"}>
                   <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
                     New Item <MdAdd />
--- src/Components/MenuContainer.jsx
+++ src/Components/MenuContainer.jsx
@@ -13,7 +13,7 @@ const MenuContainer = () => {
   return (
     <section className="w-full my-6" id="menu">
       <div className="w-full flex flex-col items-center justify-center">
-        <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-16 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100 mr-auto">
+        <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-16 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-pink-400 to-pink-600 transition-all ease-in-out duration-100 mr-auto">
           Our Hot Dishes
         </p>
 
@@ -24,15 +24,15 @@ const MenuContainer = () => {
                 whileTap={{ scale: 0.75 }}
                 key={category.id}
                 className={`group ${
-                  filter === category.urlParamName ? "bg-cartNumBg" : "bg-card"
-                } w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center hover:bg-cartNumBg `}
+                  filter === category.urlParamName ? "bg-pink-600" : "bg-card"
+                } w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center hover:bg-pink-600 `}
                 onClick={() => setFilter(category.urlParamName)}
               >
                 <div
                   className={`w-10 h-10 rounded-full shadow-lg ${
                     filter === category.urlParamName
                       ? "bg-white"
-                      : "bg-cartNumBg"
+                      : "bg-pink-600"
                   } group-hover:bg-white flex items-center justify-center`}
                 >
                   <IoFastFood
